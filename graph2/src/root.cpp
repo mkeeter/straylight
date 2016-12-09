236b00a908a1a02384b3ab99eb426c3561777114
Matt Keeter <matt.j.keeter@gmail.com>
2016-12-09 08:52:16 -0500

    Implement envsOf

--- graph2/src/root.cpp
+++ graph2/src/root.cpp
@@ -12,6 +12,8 @@ bool Root::isCell(const NameKey& k) const
 const Instance* Root::getInstance(const std::list<ItemIndex>& i) const
 {
     const Instance* target = instance.get();
+
+    // Descend into the graph!
     for (auto index : i)
     {
         target = lib.at(target->sheet).at(index).instance();
@@ -26,4 +28,46 @@ CellKey Root::toCellKey(const NameKey& k) const
     auto sheet = lib.at(i->sheet);
 
     return {k.first, sheet.indexOf(k.second)};
+}
+
+std::list<Env> Root::envsOf(const SheetIndex& s) const
+{
+    std::list<std::pair<Env, SheetIndex>> todo;
+    std::list<Env> found;
+
+    for (auto i : lib.at(0).iterItems())
+    {
+        if (auto instance = lib.at(0).at(i).instance())
+        {
+            todo.push_back({{i}, instance->sheet});
+        }
+    }
+
+    while (todo.size())
+    {
+        const auto t = todo.front();
+        todo.pop_front();
+
+        const auto env = t.first;
+        const auto sheet = t.second;
+
+        if (sheet == s)
+        {
+            found.push_back(env);
+        }
+        else
+        {
+            for (auto i : lib.at(sheet).iterItems())
+            {
+                auto env_ = env;
+                if (auto instance = lib.at(sheet).at(i).instance())
+                {
+                    env_.push_back(i);
+                    todo.push_back({env_, instance->sheet});
+                }
+            }
+        }
+    }
+
+    return found;
 }
