7bb8f6b89275fe7391ca48d0783bec7c97ab6dd8
Matt Keeter <matt.j.keeter@gmail.com>
2016-12-12 20:48:42 -0500

    Add test cases to Tree

--- graph2/test/tree.cpp
+++ graph2/test/tree.cpp
@@ -107,3 +107,93 @@ TEST_CASE("Tree::nameOf")
     REQUIRE(s.nameOf(b) == "b");
 }
 
+TEST_CASE("Tree::iterItems")
+{
+    Tree t;
+
+    SECTION("Top level")
+    {
+        auto a = t.insertInstance("a", 0, 0);
+        auto b = t.insertCell("b", "(+ 1 2)", 0);
+        auto iter = t.iterItems(0);
+        REQUIRE(iter.size() == 2);
+        REQUIRE(iter.front() == 1); // a
+        iter.pop_front();
+        REQUIRE(iter.front() == 2); // b
+
+        auto i1 = t.iterItems(1);
+        REQUIRE(i1.size() == 0);
+    }
+
+    SECTION("Nested")
+    {
+        auto a = t.insertInstance("a", 0, 1);
+        auto b = t.insertCell("b", "(+ 1 2)", 2);
+
+        auto i0 = t.iterItems(0);
+        REQUIRE(i0.size() == 0);
+
+        auto i1 = t.iterItems(1);
+        REQUIRE(i1.size() == 1);
+        REQUIRE(i1.front() == 1); // a
+
+        auto i2 = t.iterItems(2);
+        REQUIRE(i2.size() == 1);
+        REQUIRE(i2.front() == 2); // b
+    }
+}
+
+TEST_CASE("Tree::parentOf")
+{
+    Tree t;
+
+    auto a = t.insertInstance("a", 0, 1);
+    auto b = t.insertCell("b", "(+ 1 2)", 2);
+
+    REQUIRE(t.parentOf(a) == 1);
+    REQUIRE(t.parentOf(b) == 2);
+}
+
+TEST_CASE("Tree::envsOf")
+{
+    Tree t;
+    SheetIndex sheet = 15; // dummy value
+
+    SECTION("Single instance")
+    {
+        auto a = t.insertInstance("a", sheet, 0);
+
+        auto envs = t.envsOf(sheet);
+        REQUIRE(envs.size() == 1);
+
+        auto f = envs.front();
+        REQUIRE(f.size() == 2);
+        REQUIRE(f.front() == 0);
+        f.pop_front();
+        REQUIRE(f.front() == a);
+    }
+
+    SECTION("Nested instances")
+    {
+        // Here, we create a 2-layer tree, with 2 then 3 branches
+        SheetIndex parent = 57; // another dummy value
+        auto i1 = t.insertInstance("a", sheet, parent);
+        auto i2 = t.insertInstance("b", sheet, parent);
+        auto i3 = t.insertInstance("c", sheet, parent);
+        REQUIRE(i1 != i2);
+
+        auto a = t.insertInstance("a", parent, 0);
+        auto b = t.insertInstance("b", parent, 0);
+
+        {
+            auto envs = t.envsOf(sheet);
+            REQUIRE(envs.size() == 6);
+            REQUIRE(envs.front().size() == 3);
+        }
+        {
+            auto envs = t.envsOf(parent);
+            REQUIRE(envs.size() == 2);
+            REQUIRE(envs.front().size() == 2);
+        }
+    }
+}
