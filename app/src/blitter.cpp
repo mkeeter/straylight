8a93a03373eb1af99bb7c45eeb39dd5762b7b044
Matt Keeter <matt.j.keeter@gmail.com>
2017-01-08 18:17:53 -0500

    Add missing vertex attrib enabling

--- app/src/blitter.cpp
+++ app/src/blitter.cpp
@@ -26,6 +26,7 @@ Blitter::Blitter()
         glVertexAttribPointer(
                 0, 3, GL_FLOAT, GL_FALSE,
                 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
 
         quad_vbo.release();
         quad_vao.release();
