64ba8baec046f867938044907f8a4e6f44fc68dc
Matt Keeter <matt.j.keeter@gmail.com>
2017-02-19 17:47:50 -0500

    Fix older enum values

--- app/src/bridge/graph.cpp
+++ app/src/bridge/graph.cpp
@@ -19,10 +19,9 @@ void GraphModel::operator()(const Graph::Response& r)
         // Item-level operations
         case Graph::Response::ITEM_RENAMED:
         case Graph::Response::CELL_INSERTED:
-        case Graph::Response::CELL_ERASED:
+        case Graph::Response::ITEM_ERASED:
         case Graph::Response::EXPR_CHANGED:
         case Graph::Response::INSTANCE_INSERTED:
-        case Graph::Response::INSTANCE_ERASED:
         case Graph::Response::VALUE_CHANGED:
         case Graph::Response::RESULT_CHANGED:
             (*items.at(r.env))(r);
@@ -32,8 +31,7 @@ void GraphModel::operator()(const Graph::Response& r)
         case Graph::Response::INPUT_CHANGED:
         case Graph::Response::INPUT_CREATED:
         case Graph::Response::OUTPUT_CREATED:
-        case Graph::Response::INPUT_DELETED:
-        case Graph::Response::OUTPUT_DELETED:
+        case Graph::Response::IO_DELETED:
         {
             auto env_ = r.env;
             env_.pop_back();
@@ -49,6 +47,7 @@ void GraphModel::operator()(const Graph::Response& r)
         case Graph::Response::REDO_READY:
         case Graph::Response::UNDO_NOT_READY:
         case Graph::Response::REDO_NOT_READY:
+            // TODO
 
         case Graph::Response::CLEAR:
             assert(false);
