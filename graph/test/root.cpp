9edbd589a92693e0f030d6fb8ddc3e306de1d340
Matt Keeter <matt.j.keeter@gmail.com>
2017-02-21 20:21:03 -0500

    Make test pass again

--- graph/test/root.cpp
+++ graph/test/root.cpp
@@ -630,17 +630,18 @@ TEST_CASE("Root::run")
 
     shared_queue<Command> in;
     auto& out = r.run(in);
+    sleep(1);
+    while(out.size())
+    {
+        out.pop();
+    }
 
     SECTION("Inserting cell")
     {
+        REQUIRE(out.size() == 0);
         in.push(Command::InsertCell(Tree::ROOT_SHEET, "a", "(+ 1 2)"));
-        in.halt();
         sleep(1);
         REQUIRE(out.size() == 3);
-        {
-            auto a = out.pop();
-            REQUIRE(a.op == Response::INSTANCE_INSERTED); // root instance
-        }
         {
             auto a = out.pop();
             REQUIRE(a.op == Response::CELL_INSERTED);
@@ -648,9 +649,15 @@ TEST_CASE("Root::run")
         }
         {
             auto a = out.pop();
+            REQUIRE(a.op == Response::CELL_TYPE_CHANGED);
+        }
+        {
+            auto a = out.pop();
             REQUIRE(a.op == Response::VALUE_CHANGED);
             REQUIRE(a.expr == "3");
         }
     }
+    in.halt();
+    r.wait();
 }
 
