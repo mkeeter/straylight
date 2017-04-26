cbde41cf9789e60e10ac31ba7cbcf61ebc955cde
Matt Keeter <matt.j.keeter@gmail.com>
2017-04-25 21:32:16 -0400

    Add failing unit test for corners on quadtree corners

--- kernel/test/quadtree.cpp
+++ kernel/test/quadtree.cpp
@@ -66,10 +66,69 @@ TEST_CASE("Quadtree: vertex positioning")
         {
             float r = glm::length(o->getVertex());
             rmax = std::max(r, rmax);
-            rmin = std::max(r, rmin);
+            rmin = std::min(r, rmin);
         }
     }
 
     REQUIRE(rmin > radius*0.9);
     REQUIRE(rmax < radius*1.1);
+}
+
+TEST_CASE("Quadtree: vertex positioning (with corners)")
+{
+    const float radius = 0.5;
+    Tree rect = max(max(-0.5 - Tree::X(), Tree::X() - 0.5),
+                    max(-0.5 - Tree::Y(), Tree::Y() - 0.5));
+    Tree t = max(Tree::Z(), rect);
+
+    Region r({-1, 1}, {-1, 1}, {0, 0}, 4);
+
+    std::unique_ptr<Quadtree> out(Quadtree::render(t, r));
+    int found = 0;
+
+    // Queue of octrees to process
+    std::list<const Quadtree*> targets = {out.get()};
+    while (targets.size())
+    {
+        const Quadtree* o = targets.front();
+        targets.pop_front();
+
+        if (o->getType() == Quadtree::BRANCH)
+        {
+            for (unsigned i=0; i < 4; ++i)
+            {
+                targets.push_back(o->child(i));
+            }
+        }
+        else if (o->getType() == Quadtree::LEAF)
+        {
+            auto v = o->getVertex();
+            std::cout << v.x << ' ' << v.y << ' ' << v.z << '\n';
+            if (v.x < 0)
+            {
+                REQUIRE(v.x > -0.55);
+                REQUIRE(v.x < -0.45);
+            }
+            else if (v.x < 0)
+            {
+                REQUIRE(v.x > 0.45);
+                REQUIRE(v.x < 0.55);
+            }
+
+            if (v.y < 0)
+            {
+                REQUIRE(v.y > -0.55);
+                REQUIRE(v.y < -0.45);
+            }
+            else if (v.y < 0)
+            {
+                REQUIRE(v.y > 0.45);
+                REQUIRE(v.y < 0.55);
+            }
+
+            found++;
+        }
+    }
+
+    REQUIRE(found == 4);
 }
